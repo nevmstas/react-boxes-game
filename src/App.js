01223f71849dfbeef4e1ccd169d5746fe503af81
Stas <nevmyvakastas@gmail.com>
2020-04-25 14:18:00 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+//import './Header/Header'
+
+const App = () => {
+  return(
+    <div>
+      <Header />     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return(
+        <div>
+            <ul>
+                <li>Game</li>
+                <li>Rules</li>
+                <li>Abouut</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
