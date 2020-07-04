24311902ffc867cc2207032a51adb39cee0393f5
Stas <nevmyvakastas@gmail.com>
2020-07-04 17:58:42 +0600

    Updates

---
+++ precache-manifest.176bb96bf79b8c4765bbe99113f17aae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb4c802edb1b6cb1bdb6cb2d715c265",
+    "url": "/react-boxes-game/index.html"
+  },
+  {
+    "revision": "faee92127a1413d2701c",
+    "url": "/react-boxes-game/static/css/main.4960f358.chunk.css"
+  },
+  {
+    "revision": "a56bf43ada0080b62562",
+    "url": "/react-boxes-game/static/js/2.18c1c479.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-boxes-game/static/js/2.18c1c479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faee92127a1413d2701c",
+    "url": "/react-boxes-game/static/js/main.add2bbfc.chunk.js"
+  },
+  {
+    "revision": "63a70d30d2ab7b76967c",
+    "url": "/react-boxes-game/static/js/runtime-main.4bc65367.js"
+  },
+  {
+    "revision": "53941a1fb0db54031e2d369fa7534c72",
+    "url": "/react-boxes-game/static/media/dannyghost.53941a1f.png"
+  },
+  {
+    "revision": "7ae79fb278d687a2316c107acbe2cd5b",
+    "url": "/react-boxes-game/static/media/gameExample2.7ae79fb2.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-boxes-game/precache-manifest.176bb96bf79b8c4765bbe99113f17aae.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-boxes-game/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
